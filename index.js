4ce2e4c57703ae67b813e8d9d7ec955382405f3a
veerasudhan <sudhancheckmi98@gmail.com>
2021-06-14 14:50:47 +0530

    lastest commit

--- index.js
+++ index.js
@@ -21,10 +21,14 @@ app.use("/posts", postRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    process.env.MONGODB_URL ||
+      "mongodb+srv://precily:sudhan_98@cluster0.fuu3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
   })
